4fb0162501c594c6bbdd1548f3014d0aa251b6b5
KalaKriti Events <kalakritievents07@gmail.com>
2026-04-19 14:01:40 +0530

    fix: increase payload limit to 50mb to resolve 413 error

--- sm-gadgets-backend/server.js
+++ sm-gadgets-backend/server.js
@@ -26,8 +26,8 @@ app.use(cors({
   },
   credentials: true
 }));
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // routes
 app.use("/api/auth", require("./routes/auth"));
